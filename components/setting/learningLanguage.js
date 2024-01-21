c8f1f6bf811707207d9413bcff677779b5a275a5
Sangrak Choi <kars@kargn.as>
2024-01-22 08:22:44 +0900

    한국어 반말 높임말 추가

--- components/setting/learningLanguage.js
+++ components/setting/learningLanguage.js
@@ -11,8 +11,9 @@ const LANGUAGES = [
     { key: 2, value: 'Mandarin', text: 'Mandarin (Mainland China)' },
     { key: 3, value: 'Cantonese', text: 'Cantonese (Guangdong, Hong Kong)' },
     { key: 4, value: 'Japanese', text: 'Japanese' },
-    { key: 5, value: 'Korean', text: 'Korean' },
-    { key: 6, value: 'English', text: 'English (Beta)' },
+    { key: 5, value: 'Korean (Polite)', text: 'Korean (Polite)' },
+    { key: 5, value: 'Korean (Casual, Inpolite)', text: 'Korean (Casual, Inpolite) - Beta', description: 'Consider using the Advanced AI.' },
+    { key: 6, value: 'English', text: 'English - Beta' },
 ];
 
 export default function LearningLanguage() {
@@ -45,6 +46,7 @@ export default function LearningLanguage() {
                     <List.Item
                         key={item.key}
                         title={item.text}
+                        description={item.description}
                         right={props =>
                             <Checkbox
                                 status={item.value === selectedLanguage ? 'checked' : 'unchecked'}
